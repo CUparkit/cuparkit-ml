41f5abdbfb03c3516f00c5debbd12650081632f3
csteinmetz1 <csteinmetz1@gmail.com>
2019-01-27 04:16:54 -0500

    this can make predictions that I think make sense

--- model/predictor.py
+++ model/predictor.py
@@ -34,29 +34,30 @@ dt = datetime.datetime.now()
 month = to_categorical(dt.month, num_classes=12)
 day = to_categorical(dt.weekday(), num_classes=7)
 
-hours = np.arange(0, 23)
-lots = np.arange(0, 17)
+hours = np.arange(0, 24)
+lots = np.arange(0, 18)
 
-print("Generating feature vectors for each lot")
+print("Generating feature vectors and predictions for each lot")
 for lot in lots:
+	old_vecs = []
+	new_vecs = []
 	lot = to_categorical(lot, num_classes=18)
 	for hour in hours:
 		hour = to_categorical(hour, num_classes=24)
-		feature_vector = np.concatenate((month, day, hour, lot))
-		feature_vectors.append(feature_vector)
-
-# generate input sequences of len = time_lag
-time_lag = 24
-step_size = 1
-n_steps = int((X.shape[0]/step_size)-time_lag-1)
-X_seq = []
-y_seq = []
-for i in range(n_steps):
-	X_seq.append(X[i+1*step_size:(i+1*step_size+time_lag)])
+		new_vecs.append(np.concatenate((month, day, hour, lot)))
+		old_vecs.append(np.concatenate((old_month, old_day, hour, lot)))
+	X = np.array(old_vecs + new_vecs)
 
 
-print("Generating predictions...")
-X_seq = np.array(X_seq)
-print(X_seq.shape)
-preds = model.predict(X_seq)
-#print(preds.flatten())
+	# generate input sequences of len = time_lag
+	time_lag = 24
+	step_size = 1
+	n_steps = 24
+	X_seq = []
+
+	for i in range(n_steps):
+		X_seq.append(X[i+1:(i+1+time_lag)])
+
+	X_seq = np.array(X_seq)
+	preds = model.predict(X_seq)
+	print(preds.flatten())
